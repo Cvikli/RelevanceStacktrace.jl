f9bad7f5bfa06ca0194a3b90a2b58f8890002a1c
Marcell <havlikmarcell@gmail.com>
2022-08-21 11:58:12 +0200

    new tests

--- test/RelevanceStacktrace.test.jl
+++ test/RelevanceStacktrace.test.jl
@@ -4,7 +4,8 @@
 # using RelevanceStacktrace
 using Revise
 includet("../src/RelevanceStacktrace.jl")
-#%%
+
+
 func4(x) = x.+ ["32",3]
 func3(x) = x+5 + func4(x)
 func2(x) = x+3 + func3(x)
@@ -13,6 +14,19 @@ func1(x) = func2(x)
 func1(3)
 
 #%%
+try
+	eval(Meta.parse("invalidfn()"))
+catch e
+	# @edit showerror(stdout, e, catch_backtrace())
+	# showerror(stdout, e)
+	@edit Base.show_backtrace(stdout, catch_backtrace())
+end
+#%%
+
+Base.show_full_backtrace(io, filtered; print_linebreaks = stacktrace_linebreaks())
+#%%
+#%%
+
 
 func4(x) = x.+ @assert false
 
